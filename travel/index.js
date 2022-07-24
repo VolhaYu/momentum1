11df4baa5693bbc7c550d413e745df2604e386fd
VolhaYu <olenka_glushko1993@mail.ru>
2022-07-24 23:59:30 +0300

    fix: ix slider

--- travel/index.js
+++ travel/index.js
@@ -28,6 +28,8 @@ document.addEventListener('click', e => {
     }    
 });
 
+  // pop-up
+
 const login = document.querySelectorAll('.login-click');
 const popup = document.querySelector('.pop-up');
 const popupcontent = document.querySelector('.pop-up-content');
@@ -58,6 +60,9 @@ signIn.addEventListener('click', () => {
     
 });
 
+  // slider
+  // Desktop
+
 const slider = document.querySelector('.wrap-destinations');
 const slides = document.querySelectorAll('.destination');
 const slide1 = document.querySelector('.destination1');
@@ -68,30 +73,26 @@ const arrowRight = document.querySelectorAll('.arrow-right');
 const ellipse = document.querySelectorAll('.ellipse');
 
 slide1.addEventListener('click', () => {
-    slider.classList.add('wrap-destinations-active1');
-    slider.classList.remove('wrap-destinations-active2');
+    slide2.classList.add('destination2-activ');
     ellipse[0].classList.add('ellipse-active');
-    ellipse[0].classList.remove('ellipse-non-active');
-    ellipse[1].classList.toggle('ellipse-non-active');
-    ellipse[2].classList.toggle('ellipse-non-active', 'ellipse-active');
+    ellipse[1].classList.add('ellipse-non-active');
 });
 slide2.addEventListener('click', () => {
-    slider.classList.remove('wrap-destinations-active1');
-    slider.classList.toggle('wrap-destinations-active2');
-    slider.classList.remove('wrap-destinations-active3');
+    slide3.classList.add('destination3-activ');
     ellipse[1].classList.add('ellipse-active');
     ellipse[1].classList.remove('ellipse-non-active');
-    ellipse[0].classList.toggle('ellipse-non-active', 'ellipse-active');
-    ellipse[2].classList.toggle('ellipse-non-active', 'ellipse-active');
+    ellipse[0].classList.remove('ellipse-active');
+    ellipse[2].classList.remove('ellipse-active');
 });
 slide3.addEventListener('click', () => {
-    slider.classList.remove('wrap-destinations-active2');
-    slider.classList.toggle('wrap-destinations-active3');
+    slide1.classList.add('destination1-activ');
+    slide2.classList.add('destination2-activ');
     ellipse[2].classList.add('ellipse-active');
-    ellipse[2].classList.remove('ellipse-non-active');
-    ellipse[0].classList.toggle('ellipse-non-active', 'ellipse-active');
-    ellipse[1].classList.toggle('ellipse-non-active');
+    ellipse[1].classList.remove('ellipse-active');
+    ellipse[0].classList.remove('ellipse-active');
 });
+
+  // mobile
 
 let index = 0;
 const activeSlide = n => {
@@ -136,48 +137,14 @@ const prevSlide = () => {
 
 arrowRight[0].addEventListener('click', nextSlide);
 arrowLeft[0].addEventListener('click', prevSlide);
-// for(let i = 0; i <= 2; i++) {
-//     if(i = 1) {
-//         arrowRight[0].addEventListener('click', () => {
-//             slider.classList.add('wrap-destinations-active');
-//             slide3.classList.add('destination3-non');
-//         });
-//     }
-// }
-
-// arrowRight[0].addEventListener('click', () => {
-//     slider.classList.add('wrap-destinations-active');
-//     slide3.classList.add('destination3-non');
-// });
-
-    // arrowRight[0].addEventListener('click', () => {
-    //     slide2.classList.add('destination-active');
-    //     slide1.classList.add('destination-non-active');
-    //     slide3.classList.add('destination-non-active');
-    // });
-    // arrowRight[0].addEventListener('click', () => {
-    //     slide3.classList.remove('destination-active');
-    //     slide2.classList.remove('destination-non-active', 'destination-active');
-    //  });
 
 
-console.log(arrowRight);
-
-console.log(`Score 85/75\n
-1. Вёрстка соответствует макету.Ширина экрана 390px +48\n
-    -блок <header> +6\n
-    -секция preview +9\n
-    -секция steps +9\n
-    -секция destinations +9\n
-    -секция stories +9\n
-    -блок <footer> +6\n
-2. Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +15\n
-    -нет полосы прокрутки при ширине страницы от 1440рх до 390px +7\n
-    -нет полосы прокрутки при ширине страницы от 390px до 320рх +8\n
-3.На ширине экрана 390рх и меньше реализовано адаптивное меню +22\n
-    -при ширине страницы 390рх панель навигации скрывается, появляется бургер-иконка +2\n
-    -при нажатии на бургер-иконку плавно появляется адаптивное меню +4\n
-    -адаптивное меню соответствует макету +4\n
-    -при нажатии на крестик адаптивное меню плавно скрывается уезжая за экран +4\n
-    -ссылки в адаптивном меню работают, обеспечивая плавную прокрутку по якорям +4 (все кроме Account, она пока что просто закрывает меню)\n
-    при клике по ссылке в адаптивном меню адаптивное меню плавно скрывается, также скрытие меню происходит если сделать клик вне данного окна +4`)
+console.log(`Score 125/100\n
+1. Слайдер изображений в секции destinations +50\n
+    -на десктоп варианте при клике на урезанную картинку слева или справа изображение меняется по принципу карусели (например если нажать правую картинку та что была в центре на уезжает налево, а та что была видна наполовину оказывается справа). Слайдер может быть как конечным так и бесконечным - данный критерий не должен влиять на оценку + 20\n
+    -Три точки внизу отображают "номер слайда", то есть каждому слайду соответствует свой кружочек, который становится активным при нахождении соответствующего ему слайда в центре. На мобильном варианте картинка одна, но поверх нее появляются стрелочки навигации (можно сделать как карусель или же затемнять кнопку если слайдер достиг края) +20\n
+    -Анимации плавного перемещения для слайдера +10\n
+2. Нажатие на кнопку Login (кнопка Account в мобильной версии) показывает сверстанный логин попап + 50\n
+    -логин попап соответствует верстке его закрытие происходит при клике вне попапа +25\n
+    -логин попап имеет 2 инпута (email и пароль) при нажатии на кнопку Sign In показывается браузерный алерт с введенными данными (для реализации можно использовать тег ) +25\n
+3.Нажатие на кнопку Register на Login попапе меняет разметку попапа на разметку Sign Up попапа согласно макету (То есть нажатие не закрывает модал а просто меняет его наполнение). +25`);
